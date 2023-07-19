7c61aa79b82c69480ef1b1057ea64bbf5e27f8c0
MostafaRM7 <mostafakooti2018@gmail.com>
2023-07-19 15:50:03 +0330

    - result app permission fix

--- porsline_config/result_app/permissions.py
+++ porsline_config/result_app/permissions.py
@@ -1,9 +1,8 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.permissions import BasePermission
-from question_app.models import Questionnaire
 
 
 class IsQuestionnaireOwner(BasePermission):
     def has_permission(self, request, view):
         uuid = view.kwargs.get('questionnaire_uuid')
-        return request.user == get_object_or_404(Questionnaire, uuid=uuid).owner or request.user.is_staff
+        user = request.user
+        return user.questionnaires.filter(uuid=uuid).exists() or user.is_staff
