9994ed149a35a6e5253d422f30ea4b3f102c348d
MostafaRM7 <mostafakooti2018@gmail.com>
2023-07-20 16:21:56 +0330

    - permissions bug fix

--- porsline_config/question_app/permissions.py
+++ porsline_config/question_app/permissions.py
@@ -7,7 +7,7 @@ class IsQuestionnaireOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         uuid = view.kwargs.get('uuid')
         if uuid:
-            return request.user == get_object_or_404(Questionnaire, uuid=uuid).owner or request.user.is_staff
+            return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
         else:
             if request.method == 'POST':
                 return request.user.is_authenticated
@@ -17,50 +17,52 @@ class IsQuestionnaireOwnerOrReadOnly(BasePermission):
 
 class IsQuestionOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        question_id = view.kwargs.get('id')
-        questionnaire = get_object_or_404(Questionnaire, uuid=view.kwargs.get('questionnaire_uuid'))
+        question_id = view.kwargs.get('pk')
+        uuid = view.kwargs.get('questionnaire_uuid')
         if question_id:
             if request.user.is_authenticated:
-                return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+                return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
         else:
             if request.user.is_authenticated:
-                return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+                return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
 
 
 class ChangePlacementForOwnerOrStaff(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
-            questionnaire = get_object_or_404(Questionnaire, uuid=view.kwargs.get('questionnaire_uuid'))
+            uuid = view.kwargs.get('questionnaire_uuid')
             if request.user.is_authenticated:
-                return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+                return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
         return False
 
 
 class AnonPOSTOrOwner(BasePermission):
     def has_permission(self, request, view):
-        answer_set_id = view.kwargs.get('id')
-        questionnaire = get_object_or_404(Questionnaire, uuid=view.kwargs.get('questionnaire_uuid'))
+        answer_set_id = view.kwargs.get('pk')
+        uuid = view.kwargs.get('questionnaire_uuid')
         if answer_set_id:
-            if request.user.is_authenticated:
-                return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+            if request.user.is_authenticated and request.method != 'POST':
+                return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
+            else:
+                return True
         else:
             if request.method == 'POST':
                 return True
             else:
                 if request.user.is_authenticated:
-                    return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+                    return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
 
 
 class IsPageOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        page_id = view.kwargs.get('id')
-        questionnaire = get_object_or_404(Questionnaire, uuid=view.kwargs.get('questionnaire_uuid'))
+        page_id = view.kwargs.get('pk')
+        uuid = view.kwargs.get('questionnaire_uuid')
         if page_id:
             if request.user.is_authenticated:
-                return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+                return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
         else:
             if request.method == 'POST':
                 return request.user.is_authenticated
             else:
                 if request.user.is_authenticated:
-                    return questionnaire in request.user.questionnaires.all() or request.user.is_staff
+                    return request.user.questionnaires.filter(uuid=uuid).exists() or request.user.is_staff
