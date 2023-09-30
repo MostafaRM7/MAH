17ba4cda13ff6b2a0824aa4ab2b5a10201512175
MostafaRM7 <mostafakooti2018@gmail.com>
2023-09-30 17:01:22 +0330

    models added

--- user_app/models.py
+++ user_app/models.py
@@ -7,11 +7,17 @@ from django.core.validators import RegexValidator
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('i', 'پرسشگر'),
+        ('e', 'کارفرما'),
+        ('ie', 'پرسشگر و کارفرما'),
+    )
     phone_number = models.CharField(max_length=20, validators=[
         RegexValidator(regex='^09[0-9]{9}$', message='شماره تلفن همراه وارد شده صحیح نمی باشد')], unique=True,
                                     verbose_name='شماره تلفن همراه')
     username = None
     USERNAME_FIELD = 'phone_number'
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES, verbose_name='نقش')
 
 
 class OTPToken(models.Model):
@@ -25,3 +31,122 @@ class OTPToken(models.Model):
 
     def __str__(self):
         return f'{self.user} - {self.token}'
+
+
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ('m', 'مرد'),
+        ('f', 'زن'),
+    )
+    owner = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile',
+                                 verbose_name='مالک')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='جنسیت')
+    address = models.TextField(verbose_name='آدرس')
+    birth_date = models.DateField(verbose_name='تاریخ تولد', null=True, blank=True)
+    nationality = models.ForeignKey('Country', on_delete=models.CASCADE, verbose_name='ملیت')
+    province = models.ForeignKey('Province', on_delete=models.CASCADE, verbose_name='استان')
+    prefered_districts = models.ManyToManyField('District', verbose_name='مناطق مورد علاقه', blank=True)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, verbose_name='نام کشور', unique=True)
+
+
+class Province(models.Model):
+    name = models.CharField(max_length=50, verbose_name='نام استان', unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name='کشور')
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='نام شهر', unique=True)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, verbose_name='استان')
+
+
+class District(models.Model):
+    name = models.CharField(max_length=50, verbose_name='نام منطقه')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='شهر')
+
+
+class UserRoleApproveQueue(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_role_approve_queue',
+                             verbose_name='کاربر')
+    role = models.CharField(max_length=1, choices=User.ROLE_CHOICES, verbose_name='نقش')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='زمان ایجاد')
+    approved_at = models.DateTimeField(null=True, blank=True, verbose_name='زمان تایید')
+    is_approved = models.BooleanField(default=False, verbose_name='تایید شده')
+
+    def __str__(self):
+        return f'{self.user} - {self.role} - {self.created_at} - {self.is_approved}'
+
+
+class Resume(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, verbose_name='پروفایل')
+    linkedin = models.URLField(verbose_name='لینکدین', null=True, blank=True)
+    file = models.FileField(upload_to='resume', verbose_name='فایل رزومه', null=True, blank=True)
+
+
+class WorkBackground(models.Model):
+    company = models.CharField(max_length=50, verbose_name='نام شرکت')
+    position = models.CharField(max_length=50, verbose_name='عنوان شغلی')
+    start_date = models.DateField(verbose_name='تاریخ شروع')
+    end_date = models.DateField(verbose_name='تاریخ پایان')
+    description = models.TextField(verbose_name='توضیحات')
+    resume = models.ForeignKey('Resume', on_delete=models.CASCADE, verbose_name='رزومه',
+                               related_name='work_backgrounds')
+
+    def __str__(self):
+        return f'{self.resume} - {self.company} - {self.position}'
+
+
+class EducationalBackground(models.Model):
+    DEGREE_CHOICES = (
+        ('d', 'دیپلم'),
+        ('b', 'کارشناسی'),
+        ('m', 'کارشناسی ارشد'),
+        ('p', 'دکتری'),
+    ),
+    EDU_TYPE_CHOICES = (
+        ('u', 'دانشگاهی'),
+        ('h', 'حوزوی'),
+        ('o', 'سایر')
+    )
+    degree = models.CharField(max_length=1, choices=DEGREE_CHOICES, verbose_name='مدرک')
+    edu_type = models.CharField(max_length=1, choices=EDU_TYPE_CHOICES, default='u', verbose_name='نوع مدرک')
+    field = models.CharField(max_length=255, verbose_name='رشته')
+    start_date = models.DateField(verbose_name='تاریخ شروع')
+    end_date = models.DateField(verbose_name='تاریخ پایان')
+    resume = models.ForeignKey('Resume', on_delete=models.CASCADE, verbose_name='رزومه',
+                               related_name='educational_backgrounds')
+
+
+class Skill(models.Model):
+    LEVEL_CHOICES = (
+        (1, 'مبتدی'),
+        (2, 'متوسط'),
+        (3, 'حرفه ای'),
+        (4, 'خبره'),
+        (5, 'مسلط')
+    )
+    field = models.CharField(max_length=255, verbose_name='زمینه مهارت')
+    level = models.PositiveIntegerField(verbose_name='سطح مهارت', choices=LEVEL_CHOICES)
+    resume = models.ForeignKey('Resume', on_delete=models.CASCADE, verbose_name='رزومه', related_name='skills')
+
+    def __str__(self):
+        return f'{self.resume} - {self.field}'
+
+
+class Achievement(models.Model):
+    field = models.CharField(max_length=255, verbose_name='زمینه')
+    year = models.DateField(verbose_name='سال')
+    resume = models.ForeignKey('Resume', on_delete=models.CASCADE, verbose_name='رزومه', related_name='achievements')
+
+    def __str__(self):
+        return f'{self.resume} - {self.field}'
+
+
+class ResearchHistory(models.Model):
+    link = models.URLField(verbose_name='لینک')
+    year = models.DateField(verbose_name='سال')
+    field = models.CharField(max_length=255, verbose_name='زمینه')
+    resume = models.ForeignKey('Resume', on_delete=models.CASCADE, verbose_name='رزومه',
+                               related_name='research_histories')
