fc8fa7eaad6382f7bf50ae389718540cd79c2946
MostafaRM7 <mostafakooti2018@gmail.com>
2023-10-05 21:54:24 +0330

    - permission bug fix

--- wallet_app/permissions.py
+++ wallet_app/permissions.py
@@ -5,9 +5,14 @@ class IsWalletOwner(BasePermission):
     def has_permission(self, request, view):
         wallet_uuid = view.kwargs.get('uuid')
         if request.user.is_authenticated:
+            print(request.user.profile.wallet.uuid)
+            print(wallet_uuid)
+            print(str(request.user.profile.wallet.uuid) == wallet_uuid)
             if wallet_uuid:
                 if request.user.profile.wallet:
                     return str(request.user.profile.wallet.uuid) == wallet_uuid
+            else:
+                return True
 
 
 class IsTransactionOwner(BasePermission):
