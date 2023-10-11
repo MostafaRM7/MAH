48bfb089821e523ff860446a90c0e6f825f9aaee
MostafaRM7 <mostafakooti2018@gmail.com>
2023-10-11 12:59:57 +0330

    - bug fix

--- porsline_config/settings.py
+++ porsline_config/settings.py
@@ -154,8 +154,8 @@ REST_FRAMEWORK = {
     ],
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
 }
