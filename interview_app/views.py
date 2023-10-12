e06151216c070038b41f226d4993e296a12c47cf
MostafaRM7 <mostafakooti2018@gmail.com>
2023-10-12 16:09:37 +0330

    - bug fix

--- interview_app/views.py
+++ interview_app/views.py
@@ -1,12 +1,17 @@
 from uuid import UUID
 
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from interview_app.interview_app_serializers.general_serializers import InterviewSerializer
+from interview_app.interview_app_serializers.general_serializers import InterviewSerializer, AnswerSetSerializer
 from interview_app.models import Interview
 from porsline_config.paginators import MainPagination
+from question_app.models import AnswerSet
+from result_app.filtersets import AnswerSetFilterSet
+from result_app.permissions import IsQuestionnaireOwner
 
 
 # Create your views here.
@@ -44,3 +49,123 @@ class InterviewViewSet(viewsets.ModelViewSet):
     #     else:
     #         return Response({"detail": "پرسشنامه فعال نیست یا امکان پاسخ دهی به آن وجود ندارد"},
     #                         status.HTTP_403_FORBIDDEN)
+
+
+class AnswerSetViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = AnswerSet.objects.all()
+    serializer_class = AnswerSetSerializer
+    permission_classes = [IsQuestionnaireOwner]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AnswerSetFilterSet
+    pagination_class = MainPagination
+
+    @action(methods=['get'], detail=False, permission_classes=[IsQuestionnaireOwner],
+            filter_backends=[DjangoFilterBackend], filterset_class=AnswerSetFilterSet)
+    def search(self, request, questionnaire_uuid):
+        search = request.query_params.get('search', None)
+        if search is None:
+            return Response({'message': 'لطفا عبارت سرچ را وارد کنید'}, status=status.HTTP_400_BAD_REQUEST)
+        result = []
+        questionnaire = Interview.objects.get(uuid=questionnaire_uuid)
+        for answer_set in questionnaire.answer_sets.prefetch_related('answers', 'answers__question').all():
+            for answer in answer_set.answers.all():
+                question = answer.question
+                question_type = question.question_type
+                answer_set = answer.answer_set
+                answer_body = answer.answer
+                if answer_body:
+                    match question_type:
+                        case 'text_answer':
+                            look_up = answer_body.get('text_answer')
+                            if look_up:
+                                if search in look_up:
+                                    result.append(answer_set)
+                                    break
+                        case 'number_answer':
+                            look_up = answer_body.get('number_answer')
+                            if look_up:
+                                try:
+                                    if int(search) == look_up:
+                                        result.append(answer_set)
+                                        break
+                                except ValueError:
+                                    pass
+                        case 'integer_range':
+                            look_up = answer_body.get('integer_range')
+                            if look_up:
+                                try:
+                                    if int(search) == look_up:
+                                        result.append(answer_set)
+                                        break
+                                except ValueError:
+                                    pass
+                        case 'integer_selective':
+                            look_up = answer_body.get('integer_selective')
+                            if look_up:
+                                try:
+                                    if int(search) == look_up:
+                                        result.append(answer_set)
+                                        break
+                                except ValueError:
+                                    pass
+                        case 'email_field':
+                            look_up = answer_body.get('email_field')
+                            if look_up:
+                                if search in look_up:
+                                    result.append(answer_set)
+                                    break
+                        case 'link':
+                            look_up = answer_body.get('link')
+                            if look_up:
+                                if search in look_up:
+                                    result.append(answer_set)
+                                    break
+                        case 'optional':
+                            option_texts = [option.get('text') for option in answer_body.get('selected_options')]
+                            find = False
+                            for text in option_texts:
+                                if search in text:
+                                    find = True
+                                    break
+                            if find:
+                                result.append(answer_set)
+                                break
+                        case 'drop_down':
+                            option_texts = [option.get('text') for option in answer_body.get('selected_options')]
+                            find = False
+                            for text in option_texts:
+                                if search in text:
+                                    find = True
+                                    break
+                            if find:
+                                result.append(answer_set)
+                                break
+                        case 'sort':
+                            option_texts = [option.get('text') for option in answer_body.get('sorted_options')]
+                            find = False
+                            for text in option_texts:
+                                if search in text:
+                                    find = True
+                                    break
+                            if find:
+                                result.append(answer_set)
+                                break
+
+        page = self.paginate_queryset(result)
+        if page is not None:
+            serializer = AnswerSetSerializer(page, many=True,
+                                             context={'questionnaire_uuid': questionnaire_uuid})
+            return self.get_paginated_response(serializer.data)
+
+        # serializer = AnswerSetSerializer(result, many=True, context={'questionnaire_uuid': questionnaire_uuid})
+        # return Response(serializer.data)
+
+    def get_queryset(self):
+        queryset = AnswerSet.objects.prefetch_related('answers__question', 'answers').filter(
+            questionnaire__uuid=self.kwargs['questionnaire_uuid'])
+        return queryset
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({'questionnaire_uuid': self.kwargs.get('questionnaire_uuid')})
+        return context
