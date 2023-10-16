41c1b33ff29fb12b0f5ec19f3bea5d3516211836
MostafaRM7 <mostafakooti2018@gmail.com>
2023-10-16 22:11:03 +0330

    - pagination fixed

--- porsline_config/paginators.py
+++ porsline_config/paginators.py
@@ -1,20 +1,21 @@
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, _positive_int
 
 
 class MainPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 50
     choices = [2, 5, 7, 9, 10, 15, 20, 25, 30, 50]
+    page_size = 7
 
     def get_page_size(self, request):
         if self.page_size_query_param:
-            try:
-                choices = self.choices
-                page_size = int(request.query_params.get(self.page_size_query_param, choices[2]))
-                if page_size not in choices:
-                    page_size = choices[2]
-                return page_size
-            except Exception as e:
-                print(e)
-                return self.choices[2]
+            choices = self.choices
+            page_size = request.query_params.get(self.page_size_query_param)
+            if not page_size:
+                return self.page_size
+            else:
+                if int(page_size) in choices:
+                    return int(page_size)
+                else:
+                    return self.page_size
