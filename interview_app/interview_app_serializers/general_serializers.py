1360c42dfbc136f4dc30430edd0fc9d497a24a93
MostafaRM7 <mostafakooti2018@gmail.com>
2023-11-08 11:26:14 +0330

    - bug fix

--- interview_app/interview_app_serializers/general_serializers.py
+++ interview_app/interview_app_serializers/general_serializers.py
@@ -498,7 +498,8 @@ class InterviewSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'name', 'is_active', 'pub_date', 'end_date', 'created_at', 'owner', 'uuid', 'questions',
             'interviewers', 'approval_status',
-            'add_to_approve_queue', 'districts', 'goal_start_date', 'goal_end_date', 'answer_count_goal', 'difficulty'
+            'add_to_approve_queue', 'districts', 'goal_start_date', 'goal_end_date', 'answer_count_goal', 'difficulty',
+            'folder'
         )
         read_only_fields = ('owner', 'questions')
 
