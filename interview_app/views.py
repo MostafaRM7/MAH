d0a05c4ed5e5b623b56407fa7aa10b1c2cac6d56
MostafaRM7 <mostafakooti2018@gmail.com>
2023-11-15 12:42:21 +0330

    - hot fix

--- interview_app/views.py
+++ interview_app/views.py
@@ -55,12 +55,13 @@ class InterviewViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'], url_path='recommended-interviews', permission_classes=[IsInterviewer])
     def get_recommended_interviews(self, request, *args, **kwargs):
         queryset = Interview.objects.filter(districts__in=request.user.profile.preferred_districts.all(),
-                                            is_delete=False, is_active=True,
-                                            approval_status=Interview.SEARCHING_FOR_INTERVIEWERS).exclude(pk__in=request.user.profile.interviews.all().values_list('pk', flat=True))
+                                            is_delete=False, is_active=True
+                                            # approval_status=Interview.SEARCHING_FOR_INTERVIEWERS
+                                            ).exclude(pk__in=request.user.profile.interviews.all().values_list('pk', flat=True))
         # filter the query set that return the interviews that the user has not taken yet
-        queryset = queryset.filter(~Q(interviewers=request.user.profile))
+        # queryset = queryset.filter(~Q(interviewers=request.user.profile))
         # filter the query set that return the interviews that their current interviewrs count are blow the requiered count
-        queryset = queryset.annotate(interviewers_count=Count('interviewers')).filter(~Q(interviewers_count__lt=F('required_interviewer_count')))
+        # queryset = queryset.annotate(interviewers_count=Count('interviewers')).filter(~Q(interviewers_count__lt=F('required_interviewer_count')))
         paginated_queryset = self.paginate_queryset(queryset)
         serializer = self.get_serializer(data=paginated_queryset, many=True)
         serializer.is_valid()
