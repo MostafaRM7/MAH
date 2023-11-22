ff1594a88e6b2728bc7666507c6542b0e82488e0
MostafaRM7 <mostafakooti2018@gmail.com>
2023-11-22 18:01:35 +0330

    - role filters fixed

--- interview_app/apps.py
+++ interview_app/apps.py
@@ -6,5 +6,5 @@ class InterviewAppConfig(AppConfig):
     name = 'interview_app'
 
 
-    def ready(self):
-        import interview_app.signals
+    # def ready(self):
+    #     import interview_app.signals
