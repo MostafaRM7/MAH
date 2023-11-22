04787795d2cb85895c1486cfe81b267e3baa8d2f
MostafaRM7 <mostafakooti2018@gmail.com>
2023-11-22 17:09:04 +0330

    - question level will be set to zero when question is updated

--- interview_app/signals.py
+++ interview_app/signals.py
@@ -9,76 +9,66 @@ from question_app.models import OptionalQuestion, DropDownQuestion, SortQuestion
 def question_created(sender, instance: OptionalQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 @receiver(post_save, sender=DropDownQuestion)
 def question_created(sender, instance: DropDownQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=SortQuestion)
 def question_created(sender, instance: SortQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=TextAnswerQuestion)
 def question_created(sender, instance: TextAnswerQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=NumberAnswerQuestion)
 def question_created(sender, instance: NumberAnswerQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=IntegerRangeQuestion)
 def question_created(sender, instance: IntegerRangeQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=IntegerSelectiveQuestion)
 def question_created(sender, instance: IntegerSelectiveQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=EmailFieldQuestion)
 def question_created(sender, instance: EmailFieldQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=LinkQuestion)
 def question_created(sender, instance: LinkQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
 
 
 @receiver(post_save, sender=FileQuestion)
 def question_created(sender, instance: FileQuestion, created, **kwargs):
     print('question_created')
     if not created:
-        instance.level = 0
-        instance.save()
+        sender.objects.filter(id=instance.id).update(level=0)
