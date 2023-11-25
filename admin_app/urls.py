06ddb54924ad17a0921de7efc781c50397fbb68c
MostafaRM7 <mostafakooti2018@gmail.com>
2023-11-25 23:27:34 +0330

    - interviews added to price pack response

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -3,10 +3,10 @@ from rest_framework import routers
 from admin_app.views import InterviewViewSet, ProfileViewSet, PricePackViewSet, TicketViewSet
 
 router = routers.DefaultRouter()
-router.register(r'interviews', InterviewViewSet, basename='interviews')
-router.register(r'users', ProfileViewSet, basename='users')
+router.register(r'interviews', InterviewViewSet, basename='admin-interviews')
+router.register(r'users', ProfileViewSet, basename='admin-users')
 router.register(r'price-packs', PricePackViewSet, basename='price-packs')
-router.register(r'tickets', TicketViewSet, basename='tickets')
+router.register(r'tickets', TicketViewSet, basename='admin-tickets')
 
 urlpatterns = [
     path('', include(router.urls)),
