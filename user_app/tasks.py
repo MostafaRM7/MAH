13b8038b572e8813dcfe5e8284e544f695a8dea9
MostafaRM7 <mostafakooti2018@gmail.com>
2023-12-24 14:54:16 +0330

    OTP fixed

--- user_app/tasks.py
+++ user_app/tasks.py
@@ -11,6 +11,6 @@ def send_otp(otp_token, phone_number):
     _from = config('SMS_HOST')
     text = f"{otp_token}"
     to = phone_number
-    response = api.send_by_base_number(to=to, text=text, bodyId=120794)
+    response = api.send_by_base_number(to=to, text=text, bodyId=182413)
     print(response)
     return response
