b768dcfbaebfa9ff3fc7efa1d5308a6620939e0a
mamfyou <mamf20021381@gmail.com>
2024-04-09 13:31:14 +0330

    fixing add interviewer action to separate url

--- interview_app/views.py
+++ interview_app/views.py
@@ -45,7 +45,7 @@ class AddInterViewersViewSet(ReadOnlyModelViewSet, CreateModelMixin, DestroyMode
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
