4191ee6c0d8a021527133002f4fcd699faa986ff
aligh1307 <gharahlou@gmail.com>
2024-04-16 16:21:02 +0330

    change otp token code

--- user_app/user_app_serializers/authentication_serializers.py
+++ user_app/user_app_serializers/authentication_serializers.py
@@ -17,8 +17,8 @@ class GateWaySerializer(serializers.Serializer):
         if otp.exists():
             otp.delete()
         user = Profile.objects.get_or_create(phone_number=validated_data.get('phone_number'))
-        # otp = OTPToken.objects.create(token=randint(10000, 99999), user=user[0])
-        otp = OTPToken.objects.create(token='12345', user=user[0])
+        otp = OTPToken.objects.create(token=randint(10000, 99999), user=user[0])
+        # otp = OTPToken.objects.create(token='12345', user=user[0])
         print(otp.token)
         send_otp.delay(otp.token, validated_data.get('phone_number'))
         return validated_data
